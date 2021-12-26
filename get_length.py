a2864f1a2b96e42cf73a7e18e4bf0ae52371029d
ZARIFMAC <uzbzarif@gmail.com>
2021-12-26 11:42:11 +0900

    12

--- get_length.py
+++ get_length.py
@@ -9,7 +9,25 @@ def get_length(num):
     Returns:
         int: length of integer
     """
-    # return number of digits in integer
-    return
+    k=6
+    
+    num//=10
+    k-=pow(0,num)
+    
+    num//=10
+    k-=pow(0,num)
 
+    num//=10
+    k-=pow(0,num)
+
+    num//=10
+    k-=pow(0,num)
+
+
+    # return number of digits in integer
+    return k
+
+
+
+print(get_length(10000))
  
